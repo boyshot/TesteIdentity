1ed8c01e7406026bf19387a8d119d9ba3f121bf8
Paulo Rocha <pauloandredarocha@gmail.com>
2021-04-14 22:45:50 -0300

    Projeto Inicial Teste Identity 2

---
+++ TesteIdentityApp/TesteIdentityApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TesteIdentityApp.Areas.Identity.Data;
+using TesteIdentityApp.Identity;
+
+[assembly: HostingStartup(typeof(TesteIdentityApp.Areas.Identity.IdentityHostingStartup))]
+namespace TesteIdentityApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityAppContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("IdentityAppContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdentityAppContext>();
+            });
+        }
+    }
+}
